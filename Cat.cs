a4ae31476c5a4c366302e2212f7b4c2eb2e51397
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-06 17:15:06 -0500

    Create Cat and inherit from Animal class

--- Cat.cs
+++ Cat.cs
@@ -7,8 +7,34 @@
 
 namespace pa_inheritance_overriding
 {
-    public class Cat
+    public class Cat : Animal
     {
+        // Class Attributes
+        public string? Sound { get; set; }
 
+        // Default Constructor
+        public Cat()
+        {
+            base.Name = "";
+            base.Legs = 0;
+            Sound = "";
+        }
+
+        // Overloaded Constructor
+        public Cat(string name, int legs, string sound) : base(name, legs)
+        {
+            base.Name = name;
+            base.Legs = legs;
+            Sound = sound;
+        }
+
+        // Class Methods
+        public override void printAnimal()
+        {
+            Console.WriteLine(
+                $"The Cat's name is {base.Name}, and it has {base.Legs} and " +
+                $"is making a meow sound."
+            );
+        }
     }
 }
